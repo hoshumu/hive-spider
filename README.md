872d1068ecd6e5aed776c10a11948270b0a740e4
hoshumu <hoshumu@hoshumu.pp.ua>
2020-10-01 01:59:51 +0800

    update README

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
-**代码基于新edge的selenium**
-1. 运行getcookies.py获取登录cookies
-2. 运行爬虫程序
+# hive-spider
+
+## 项目简介
+
+本项目是用于对[招标采购导航网](https://www.okcis.cn/)的一个基于Selenium的爬虫，目前能够爬取的信息有
+
+- 招标内容
+- 项目链接
+- 所属地区
+- 中标公司
+- 中标公司链接
+- 发布日期
+- 联系人
+- 联系电话
+
+## 运行指南
+
+1. pip通过`requirements.txt`文件安装依赖，同时安装`WebDriver`
+2. 在`account.py`中填入账号信息（百度云图像识别模块暂时废弃可不填）
+3. 运行`getcookies.py`获取cookies（cookies需要每天更新）
+4. 运行`pachong.py`爬取数据，数据会自动存储到`Result.xls`
+
+### 关于断点续搜
+
+本爬虫会记录之前爬取的数据并读取已经爬取到的数据，如果需要重新爬取请删除`history.txt`文件
+
+## Contribution
+
+本项目由[@chaoers](https://github.com/chaoers)和[@uniartisan](https://github.com/uniartisan)共同完成
+
+## LICENSE
+
+本项目遵循`GPLV3.0`开源协议
--- getcookies.py
+++ getcookies.py
@@ -1,10 +1,8 @@
 # coding:utf-8
 import time
 import json
-import requests
-import urllib3
-from urllib.request import urlretrieve
 
+# selenium模块
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -13,27 +11,36 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 # 读取设置的账号密码
 import account
-import getNum
+
+# 图像识别模块（废弃）
+# import getNum
+# import urllib3
+# from urllib.request import urlretrieve
 
 driver = webdriver.Chrome()
-https = urllib3.PoolManager()
+# https = urllib3.PoolManager()
 
+# hover登录btn
 driver.get('https://www.okcis.cn/search/')  # 打开网页
 ele=driver.find_element_by_xpath("//a[@class='denglu_lia_20141021']")
 ActionChains(driver).move_to_element(ele).perform()
 
-# 定位iframe，暂时没有实现自动翻页
+# 定位iframe
 iframe_name = "site-top-login-iframe"
 iframe = driver.find_element_by_name(iframe_name)
 iframe_location = iframe.location
 driver.switch_to.frame(iframe)
 
+# 输入读取信息
 driver.find_element_by_xpath("//input[@name='username']").send_keys(account.account_phone)
 driver.find_element_by_xpath("//input[@name='password']").send_keys(account.account_password)
 
+# 手动填写二维码
 try:
     print('请在10秒内输入验证码')
     time.sleep(10)
+
+    # 图像识别模块（废弃）
     # img = driver.find_element_by_xpath("//li[@class='inde_dlli_20141021']/img[@class='yztupian_im_20141021']")
 
     # num = getNum.getCodeImg(driver, img, iframe_location['x'] + 560, iframe_location['y'])
@@ -49,6 +56,8 @@ try:
     # print(num)
 
     # driver.find_element_by_xpath("//input[@name='yzm']").send_keys(num)
+
+    # 点击登录
     driver.find_element_by_xpath("//input[@name='iframe-login-submit']").click()
 
 except:
@@ -56,6 +65,7 @@ except:
     driver.quit()
 
 else:
+    # cookies获取与储存
     cookies = driver.get_cookies()
     jsoncookies = json.dumps(cookies)
     with open('cookies.txt', 'w') as f:
